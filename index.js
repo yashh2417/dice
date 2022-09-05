bb28c46a3b7448276ec573583e31ff6ad942fba1
Yash <111681114+yashh2417@users.noreply.github.com>
2022-09-05 13:57:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
+$("h1").click(function() {
+
+  clickHeading();
+
+});
+
+function clickHeading() {
+
 // First Dice roll
 
-var randomNumber1= Math.floor(Math.random()*6)+1;
+  var randomNumber1= Math.floor(Math.random()*6)+1;
 
-document.getElementsByClassName('dice-image')[0].setAttribute("src","dices/dice"+randomNumber1+".png");
+  document.getElementsByClassName('dice-image')[0].setAttribute("src","dices/dice"+randomNumber1+".png");
 
 // Second Dice Roll
 
-var randomNumber2= Math.floor(Math.random()*6)+1;
+  var randomNumber2= Math.floor(Math.random()*6)+1;
 
-document.getElementsByClassName('dice-image')[1].setAttribute("src","dices/dice"+randomNumber2+".png");
+  document.getElementsByClassName('dice-image')[1].setAttribute("src","dices/dice"+randomNumber2+".png");
 
 // Who Wins
 
-if(randomNumber1>randomNumber2){
-  document.getElementsByClassName('big-heading')[0].innerHTML="🚩 Player 1 Wins!";
-}
-else if (randomNumber2>randomNumber1) {
-  document.getElementsByClassName('big-heading')[0].innerHTML="Player 2 Wins! 🚩";
-}
-else{
-  document.getElementsByClassName('big-heading')[0].innerHTML="Draw!";
+  if(randomNumber1>randomNumber2){
+    document.getElementsByClassName('big-heading')[0].innerHTML="🚩 Player 1 Wins!";
+  }
+  else if (randomNumber2>randomNumber1) {
+    document.getElementsByClassName('big-heading')[0].innerHTML="Player 2 Wins! 🚩";
+  }
+  else{
+    document.getElementsByClassName('big-heading')[0].innerHTML="Draw!";
+  }
 }
